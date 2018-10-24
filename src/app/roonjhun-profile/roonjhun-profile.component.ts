b2c13172e943d09b34825880851224e7ea36ce01
Roonjhun <roonjhun.agarwal@gmail.com>
2018-10-24 14:37:19 +0200

    Roonjhun Profile details

---
+++ src/app/roonjhun-profile/roonjhun-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-roonjhun-profile',
+  templateUrl: './roonjhun-profile.component.html',
+  styleUrls: ['./roonjhun-profile.component.css']
+})
+export class RoonjhunProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
